ee1f72d218896f00c5781a012434417be4db57de
Aditi Dankar <aditidankar@gmail.com>
2020-05-16 23:10:13 +0530

    feat: Added new functions
    board and player_input functions added to tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -1,1 +1,27 @@
 # This is a simple tic-tac-toe implemented using Python
+
+def board():
+    print('7' + '|' + '8' + '|' + '9')
+    print('-' + '|' + '-' + '|' + '-')
+    print('4' + '|' + '5' + '|' + '6')
+    print('-' + '|' + '-' + '|' + '-')
+    print('1' + '|' + '2' + '|' + '3')
+
+def player_input():
+
+    player1 = ''
+    player2 = ''
+
+    while player1 != 'x' and player1 != 'o':
+        player1 = input("Player 1, please pick a marker x or o: ")
+
+    if player1 == 'x':
+        player2 = 'o'
+    else:
+        player2 = 'x'
+
+    return (player1, player2)
+
+player1, player2 = player_input()
+
+print((player1, player2))
